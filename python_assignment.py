48657d490463f09578d2bba925d5923e39b953ed
Yash Dev Ladha <Yashdevladha.gmail.com>
2023-08-24 12:28:25 +0530

    msg_69

--- python_assignment.py
+++ python_assignment.py
@@ -1,66 +1,46 @@
-class Match:
-    def __init__(self, location, team1, team2, timing):
-        self.location = location
-        self.team1 = team1
-        self.team2 = team2
-        self.timing = timing
+# Yash Dev Ladha E22CSEU0068 
+class table:
+    d={1:["Mumbai","India","Sri Lanka","DAY"],
+    2:["Delhi","England","Australia","DAY-NIGHT"],
+    3:["Chennai","India","South Africa","DAY"],
+    4:["Indore","England","Sri Lanka","DAY-NIGHT"],
+    5:["Mohali","Australia","South Africa","DAY-NIGHT"],
+    6:["Delhi","India","Australia","DAY"]
+    }
+    
 
-class FlightTable:
-    def __init__(self):
-        self.matches = []
+    def match(self,name):
+        for i in table.d:
+            if(table.d[i][1]==name or table.d[i][2]==name):
+                print(table.d[i][1],"vs ",table.d[i][2])
+                
+    def location(self, loc):
+        for i in table.d:
+            if(table.d[i][0]==loc):
+                print(table.d[i][1],"vs ",table.d[i][2])
 
-    def add_match(self, location, team1, team2, timing):
-        match = Match(location, team1, team2, timing)
-        self.matches.append(match)
-
-    def search_by_team(self, team_name):
-        matches = [match for match in self.matches if team_name in (match.team1, match.team2)]
-        return matches
-
-    def search_by_location(self, location_name):
-        matches = [match for match in self.matches if match.location == location_name]
-        return matches
-
-    def search_by_timing(self, timing_type):
-        matches = [match for match in self.matches if timing_type in match.timing]
-        return matches
-
-def main():
-    flight_table = FlightTable()
-
-    flight_table.add_match("Mumbai", "India", "Sri Lanka", "DAY")
-    flight_table.add_match("Delhi", "England", "Australia", "DAY-NIGHT")
-    flight_table.add_match("Chennai", "India", "South Africa", "DAY")
-    flight_table.add_match("Indore", "England", "Sri Lanka", "DAY-NIGHT")
-    flight_table.add_match("Mohali", "Australia", "South Africa", "DAY-NIGHT")
-    flight_table.add_match("Delhi", "India", "Australia", "DAY")
-
-    while True:
-        print("Search options:")
-        print("1. List of all the matches of a Team")
-        print("2. List of Matches on a Location")
-        print("3. List of Matches based on timing")
-        print("4. Exit")
-        choice = int(input("Enter your choice: "))
-
-        if choice == 1:
-            team_name = input("Enter team name: ")
-            matches = flight_table.search_by_team(team_name)
-        elif choice == 2:
-            location_name = input("Enter location name: ")
-            matches = flight_table.search_by_location(location_name)
-        elif choice == 3:
-            timing_type = input("Enter timing type: ")
-            matches = flight_table.search_by_timing(timing_type)
-        elif choice == 4:
-            break
-        else:
-            print("Invalid choice.")
-            continue
-
-        print("Match Location\tTeam 01\tTeam 02\tTiming")
-        for match in matches:
-            print(f"{match.location}\t{match.team1}\t{match.team2}\t{match.timing}")
-
-if __name__ == "__main__":
-    main()
+    def Timing(self, day):
+        for i in table.d:
+            if(table.d[i][3]==day):
+                print(table.d[i][1],"vs ",table.d[i][2])
+    
+            
+obj = table()
+while(True):
+    print("1. List of all the matches of a Team ")
+    print("2. List of Matches on a Location") 
+    print("3. List of Matches based on timing.")
+    print("4.exit")
+    print()
+    a = int(input("enter choice  : " ))
+    if(a==1):
+        b = input("Enter team ")
+        obj.match(b)
+    elif(a==2):
+        c = input("Enter location ")
+        obj.location(c)
+    elif(a==3):
+        d = input("Enter timing ")
+        obj.Timing(d)
+    elif(a==4):
+        break
